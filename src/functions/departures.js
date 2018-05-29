a7535469f6767e0d215bfcfa2994bdbdd215837b
Stephen Lau <stephen@edument.se>
2018-05-29 16:11:59 +0200

    Add netlify function

---
+++ src/functions/departures.js
@@ -0,0 +1,14 @@
+let rp = require("request-promise");
+
+exports.handler = function(event, context, callback) {
+  let skanetrafikenUrl =
+    `http://www.labs.skanetrafiken.se/v2.2/stationresults.asp?selPointFrKey=${event.queryStringParameters.stop}`;
+
+  rp(skanetrafikenUrl)
+    .then(function (body) {
+      callback(null, {
+        statusCode: 200,
+        body
+      });
+    });
+};
